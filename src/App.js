54d47dfcf923da3166d6641bf90b2af785ad7051
Gaku <gaku.jay.jinyama@gmail.com>
2020-10-24 15:44:44 +0900

    Added profiledata

--- src/App.js
+++ src/App.js
@@ -8,19 +8,49 @@ import Resume from './components/Resume/Resume';
 import Work from './components/Work/Work';
 import Contact from './components/Contact/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Header />
-      </header>
-      <Home/>
-      <About />
-      <Resume />
-      <Work />
-      <Contact />
-    </div>
-  );
-}
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileData: {}
+    }
+  }
+
+  getProfileData = () => {
+    fetch('/profileData.json', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then(response => response.json())
+    .then(data => (
+      this.setState({profileData: data})
+    ));
+  }
+
+  componentDidMount(){
+    this.getProfileData();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Header />
+        </header>
+        <Home data={this.state.profileData.main}/>
+        <About data={this.state.profileData.main}/>
+        <Resume data={this.state.profileData.resume}/>
+        <Work data={this.state.profileData.portfolio}/>
+        <Contact data={this.state.profileData.main}/>
+      </div>
+    );
+  }
+};
+
+ 
 
 export default App;
